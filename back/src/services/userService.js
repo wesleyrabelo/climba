4bdb92267db1732f15cda6f7518d0892443153d4
Wesley Rabelo <wesleyrab2020@gmail.com>
2026-05-12 09:34:53 -0300

    Senhas de novos usuários são criptografadas

--- back/src/services/userService.js
+++ back/src/services/userService.js
@@ -1,17 +1,17 @@
 const pool = require("../db");
+const bcrypt = require("bcrypt");
 
 async function createUser(name, email, password) {
+  const saltRounds = 10;
+  const hashedPassword = await bcrypt.hash(password, saltRounds);
+
   const result = await pool.query(
     `
-      INSERT INTO users (
-        name,
-        email,
-        password
-      )
-      VALUES ($1, $2, $3)
-      RETURNING id, name, email, created_at
+    INSERT INTO users (name, email, password)
+    VALUES ($1, $2, $3)
+    RETURNING id, name, email, created_at
     `,
-    [name, email, password],
+    [name, email, hashedPassword],
   );
 
   return result.rows[0];
@@ -20,15 +20,30 @@ async function createUser(name, email, password) {
 async function loginUser(email, password) {
   const result = await pool.query(
     `
-      SELECT *
-      FROM users
-      WHERE email = $1
-      AND password = $2
+    SELECT *
+    FROM users
+    WHERE email = $1
     `,
-    [email, password],
+    [email],
   );
 
-  return result.rows[0];
+  const user = result.rows[0];
+
+  if (!user) {
+    throw new Error("Usuário não encontrado");
+  }
+
+  const passwordMatch = await bcrypt.compare(password, user.password);
+
+  if (!passwordMatch) {
+    throw new Error("Senha inválida");
+  }
+
+  return {
+    id: user.id,
+    name: user.name,
+    email: user.email,
+  };
 }
 
 module.exports = {
